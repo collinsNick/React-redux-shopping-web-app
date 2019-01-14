8d2aef5476256453de8838b41f83884861be4ede
kolloh <col.omondinick@gmail.com>
2019-01-14 15:56:02 +0300

    page layout setup complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Layout from './layout/Layout';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        home
+          <Layout></Layout>
       </div>
     );
   }
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -3,7 +3,22 @@ import React, {Component} from 'react';
 class Layout extends Component {
     render() {
         return (
-            <h2>layout page</h2>
+            <React.Fragment>
+                <div className="container-fluid">
+                    <div>nav bar</div>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-3">
+                                side bar
+                            </div>
+                            <div className="col-md-9">
+                                main content
+                            </div>
+                        </div>
+                    </div>
+                    <div>Footer</div>
+                </div>
+            </React.Fragment>
         )
     }
 }
