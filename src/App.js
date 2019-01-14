75ef5d65423b3da6a1fcc3d729a1efa84b8bf7ba
kolloh <col.omondinick@gmail.com>
2019-01-14 18:05:34 +0300

    linked menu bar cart count to store cart count

--- src/App.js
+++ src/App.js
@@ -30,4 +30,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withRouter(App);
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,42 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import {NavLink} from 'react-router-dom';
 
-const menu = () => {
+class Menu extends Component {
+    render() {
 
-    return (
+        return (
 
-        <nav className="navbar navbar-expand-md navbar-light bg-light">
-            <button className="navbar-toggler" type="button">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse">
-                <span className="navbar-brand">DUKA</span>
-                <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to="/" exact>Home <span className="sr-only">(current)</span></NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to="/men" exact>Men</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to="/women" exact>Women</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to="/children" exact>Children</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to="/sale" exact>Sale</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to="/cart" exact>Cart <span className="badge badge-light">4</span></NavLink>
-                    </li>
-                </ul>
+            <nav className="navbar navbar-expand-md navbar-light bg-light">
+                <button className="navbar-toggler" type="button">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse">
+                    <span className="navbar-brand">DUKA</span>
+                    <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/" exact>Home <span
+                                className="sr-only">(current)</span></NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/men" exact>Men</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/women" exact>Women</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/children" exact>Children</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/sale" exact>Sale</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/cart" exact>Cart <span
+                                className="badge badge-light">{this.props.cartItemNumber}</span></NavLink>
+                        </li>
+                    </ul>
 
-            </div>
-        </nav>
+                </div>
+            </nav>
 
-    )
+        )
+    }
 
 }
 
-export default menu;
+const mapStateToProps = (state) => {
+    return {
+        cartItemNumber: state.cartTotal
+    }
+}
+
+export default connect(mapStateToProps)(Menu);
