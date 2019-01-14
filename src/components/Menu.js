fb494d7ed546054abdf7f600219718a707715663
kolloh <col.omondinick@gmail.com>
2019-01-14 17:35:00 +0300

    menu and footer links complete

---
+++ src/components/Menu.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+const menu = () => {
+
+    return (
+
+        <nav className="navbar navbar-expand-md navbar-light bg-light mb-2">
+            <button className="navbar-toggler" type="button">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse">
+                <span className="navbar-brand">DUKA</span>
+                <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/" exact>Home <span className="sr-only">(current)</span></NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/men" exact>Men</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/women" exact>Women</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/children" exact>Children</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/sale" exact>Sale</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/cart" exact>Cart <span className="badge badge-light">4</span></NavLink>
+                    </li>
+                </ul>
+
+            </div>
+        </nav>
+
+    )
+
+}
+
+export default menu;
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,22 +1,32 @@
 import React, {Component} from 'react';
+import Menu from '../components/Menu'
+import Footer from '../components/Footer'
 
 class Layout extends Component {
     render() {
         return (
             <React.Fragment>
                 <div className="container-fluid">
-                    <div>nav bar</div>
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-md-3">
-                                side bar
-                            </div>
-                            <div className="col-md-9">
-                                {this.props.children}
+                    <header>
+                        <Menu />
+                    </header>
+                    <main>
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-md-3">
+                                    side bar
+                                </div>
+                                <div className="col-md-9">
+                                    {this.props.children}
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div>Footer</div>
+
+                    </main>
+
+                    <footer>
+                        <Footer />
+                    </footer>
                 </div>
             </React.Fragment>
         )
