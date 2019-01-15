0a1738f8c600eb631c1625af5763fd884485ec41
kolloh <col.omondinick@gmail.com>
2019-01-15 14:24:50 +0300

    added products to remaining pages

--- src/containers/pages/Index.jsx
+++ src/containers/pages/Index.jsx
@@ -1,37 +1,30 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import { ADD_TO_CART } from '../../store/actions/shop';
+import {ADD_TO_CART} from '../../store/actions/shop';
 import ProductCard from '../../components/ProductCard';
 
 class Index extends Component {
-
-    addToCartHandler = (id) => {
-        alert(id)
-    }
 
     render() {
 
         let products = null;
 
         if (this.props.productsProps) {
-            products = this.props.productsProps.map( product => {
+            products = this.props.productsProps.map(product => {
                 return (
                     <ProductCard
-                        key = {product.id}
-                        productName = {product.name}
-                        productPrice = {product.price}
-                        productDiscountPrice = {product.discount_price}
-                        productSale = {product.sale}
+                        key={product.id}
+                        productName={product.name}
+                        productPrice={product.price}
+                        productDiscountPrice={product.discount_price}
+                        productSale={product.sale}
                         productImage={product.img}
                         productCategory={product.category}
                         addToCart={() => this.props.addProductToCartProp(product.id)}
-                        />
+                    />
                 )
             })
-        } else {
-            products = 'mimi'
         }
-
         return (
             <div className="row">
                 {products}
@@ -48,7 +41,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        addProductToCartProp : (productId) => dispatch({ type: ADD_TO_CART, productId:productId })
+        addProductToCartProp: (productId) => dispatch({type: ADD_TO_CART, productId: productId})
     }
 }
 
--- src/containers/pages/Men.jsx
+++ src/containers/pages/Men.jsx
@@ -1,11 +1,47 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import ProductCard from '../../components/ProductCard';
+import {ADD_TO_CART} from "../../store/actions/shop";
 
 class Men extends Component {
     render() {
+
+        let products = null;
+
+        if (this.props.productsProps) {
+            products = this.props.productsProps.map(product => {
+                return (
+                    <ProductCard
+                        key={product.id}
+                        productName={product.name}
+                        productPrice={product.price}
+                        productDiscountPrice={product.discount_price}
+                        productSale={product.sale}
+                        productImage={product.img}
+                        productCategory={product.category}
+                        addToCart={() => this.props.addProductToCartProp(product.id)}
+                    />
+                )
+            })
+        }
         return (
-            <h2>men page</h2>
+            <div className="row">
+                {products}
+            </div>
         )
     }
 }
 
-export default Men;
+const mapStateToProps = state => {
+    return {
+        productsProps: state.products
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        addProductToCartProp: (productId) => dispatch({type: ADD_TO_CART, productId: productId})
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Men);
