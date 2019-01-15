f3f362a9a661e221bf929dcc3da1ee5e062ddca9
kolloh <col.omondinick@gmail.com>
2019-01-15 17:47:37 +0300

    cart page layout set

---
+++ src/components/CartProduct.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const footer = () => {
+    return(
+        <React.Fragment>
+            <div className=" shop-footer text-center py-3">made with Love by <a target={'blank'} href={'/'}>Collins nixon</a>
+                .This project is open source, visit <a target={'blank'} href={'/'}>the repo</a>
+
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default footer;
--- src/containers/pages/Cart.jsx
+++ src/containers/pages/Cart.jsx
@@ -1,21 +1,118 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import { REMOVE_FROM_CART, CLEAR_CART } from '../../store/actions/shop';
+import {REMOVE_FROM_CART, CLEAR_CART} from '../../store/actions/shop';
+import photo from '../../assets/shop_images/men/slim-fit-suit.jpg';
 
 class Cart extends Component {
     render() {
 
         let cartProduct = null;
 
-        if(this.props.cartProductProp.length){
+        if (this.props.cartProductProp.length) {
             cartProduct = <h4>You have products in your cart</h4>
-        }else{
+        } else {
             cartProduct = <h5>Your cart is empty. Please fill it up.</h5>
         }
 
         return (
             <div className={'p-4 shop-cart-div'}>
-                {cartProduct}
+
+                <div className="row">
+                    <div className="col">
+                        <div className="row">
+                            <div className="col-sm-2">
+                                <img className={'shop-cart-image'} src={photo} alt={'some-photo'}/>
+                            </div>
+                            <div className="col-sm-4">
+                                <h5 className="shop-cart-name ">Product name</h5>
+                                <h6 lassName="shop-cart-category ">
+                                    Product category
+                                </h6>
+                            </div>
+                            <div className="col-sm-6">
+                                <div className="row">
+                                    <div className="col-sm-6 text-right">
+                                        <h6 className={'shop-cart-price'}>Ksh. 25.00</h6>
+                                        <input type="text" className="form-control input-sm shop-cart-quantity my-3"/>
+                                        <h6 className={'shop-cart-total'}>Item Total <strong>Ksh. 25.00</strong></h6>
+                                    </div>
+                                    <div className="col-sm-4 offset-sm-2 text-right">
+                                        <button type="button" className="btn btn-primary btn-sm">
+                                            Remove
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr/>
+                <div className="row">
+                    <div className="col">
+                        <div className="row">
+                            <div className="col-sm-2">
+                                <img className={'shop-cart-image'} src={photo} alt={'some-photo'}/>
+                            </div>
+                            <div className="col-sm-4">
+                                <h5 className="shop-cart-name ">Product name</h5>
+                                <h6 lassName="shop-cart-category ">
+                                    Product category
+                                </h6>
+                            </div>
+                            <div className="col-sm-6">
+                                <div className="row">
+                                    <div className="col-sm-6 text-right">
+                                        <h6 className={'shop-cart-price'}>Ksh. 25.00</h6>
+                                        <input type="text" className="form-control input-sm shop-cart-quantity my-3"/>
+                                        <h6 className={'shop-cart-total'}>Item Total <strong>Ksh. 25.00</strong></h6>
+                                    </div>
+                                    <div className="col-sm-4 offset-sm-2 text-right">
+                                        <button type="button" className="btn btn-primary btn-sm">
+                                            Remove
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <hr/>
+                <div className="row">
+                    <div className="col-sm-3 offset-sm-6 text-left">
+                        Subtotal
+                    </div>
+                    <div className="col-sm-3 text-right">
+                        Ksh. 3786648
+                    </div>
+                </div>
+                <hr/>
+                <div className="row">
+                    <div className="col-sm-3 offset-sm-6 text-left">
+                        Estimated shipping
+                    </div>
+                    <div className="col-sm-3 text-right">
+                        Ksh. 3786648
+                    </div>
+                </div>
+                <hr/>
+                <div className="row">
+                    <div className="col-sm-3 offset-sm-6 text-left">
+                        <h4 className={'shop-cart-total'}>Total</h4>
+                    </div>
+                    <div className="col-sm-3 text-right">
+                        <h4 className={'shop-cart-total'}>Ksh. 345566</h4>
+                    </div>
+                </div>
+                 <hr/>
+                <div className="row">
+                    <div className="col-sm-3 offset-sm-6 text-left">
+                        <a className="btn btn-outline-primary" href="#" role="button">Continue shopping</a>
+                    </div>
+                    <div className="col-sm-3 text-right">
+                        <button className="btn btn-primary" type="submit">Checkout</button>
+                    </div>
+                </div>
             </div>
         )
     }
@@ -35,4 +132,4 @@ const mapDispatchToProps = dispatch => {
 }
 
 
-export default connect(mapStateToProps, mapDispatchToProps )(Cart);
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
