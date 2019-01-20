3229f056a4910cbaf7fb1368d1962626cb0dfd25
kolloh <col.omondinick@gmail.com>
2019-01-20 05:16:04 +0300

    continuation with responsive modifications

--- src/Layouts/SubsidiaryLayout.jsx
+++ src/Layouts/SubsidiaryLayout.jsx
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class SubsidiaryLayout extends Component {
     render() {
         return (
-            <div className="container w-75 py-4">
+            <div className="container shop-container py-4">
                 <div className={'p-4 shop-div'}>
                     {this.props.children}
                 </div>
