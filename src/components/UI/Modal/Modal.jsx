307157d05844fc816d0ae348ce7f2600696c91df
kolloh <col.omondinick@gmail.com>
2019-01-20 14:09:42 +0300

    modal v1 complete

--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -6,21 +6,21 @@ const modal = (props) => {
     return (
         <React.Fragment>
             <Backdrop showBackDrop={props.showModal} closeSomething={props.closeModalClick}/>
-            <div className={'modal fade show'} style={{display: 'block'}}>
-                <div className="modal-dialog" role="document">
-                    <div className="modal-content">
-                        <div className="modal-body">
-                            <p className={'pt-3'}>{props.children}</p>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button"
-                                    className="btn shop-btn-primary"
-                                    onClick={props.closeModalClick}>Continue Shopping
-                            </button>
+                <div className={'shop-modal fade show'} >
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-body">
+                                <p className={'pt-3'}>{props.children}</p>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button"
+                                        className="btn shop-btn-primary"
+                                        onClick={props.closeModalClick}>Continue Shopping
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
         </React.Fragment>
 
     )
